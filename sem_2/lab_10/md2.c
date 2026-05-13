394f8142062f04169149b739acc715cbd8f403c3
jur4ik <ypopov2005@gmail.com>
2026-05-13 11:42:49 +0300

    save

--- sem_2/lab_10/md2.c
+++ sem_2/lab_10/md2.c
@@ -8,6 +8,8 @@ static int __init md_init(void) {
    printk( "+ module md2 start!\n" );
    printk( "+ data string exported from md1 : %s\n", md1_data );
    printk( "+ string returned md1_proc() is : %s\n", md1_proc() );
+   printk( "+ string returned md1_proc() is : %s\n", md1_noexport() );
+
    return 0;
 }
 static void __exit md_exit(void) {
