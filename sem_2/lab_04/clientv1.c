817dc8bd2fd3b34897783a48faee1746ef00278f
jur4ik <ypopov2005@gmail.com>
2026-06-05 03:24:35 +0300

    test

--- sem_2/lab_04/clientv1.c
+++ sem_2/lab_04/clientv1.c
@@ -25,26 +25,11 @@ int main()
     struct sockaddr_in srvr_addr;
     char clnt_type = 'p', diff = 19, buf[3];
     ssize_t read_len = 0;
-    //char file_name[32];
-    //FILE *fptr;
-    //struct timespec p_starttime, p_endtime;
-    //long seconds, nanoseconds;
-    //double msec, msec_sum = 0.0;
-    //int i = 0;
-
-    //sprintf(file_name, "%d.log", getpid());
-    //fptr = fopen(file_name, "w");
-    // if (fptr == NULL)
-    // {
-    //     perror("fopen");
-    //     exit(1);
-    // }
     
     if (signal(SIGINT, sig_handler) == SIG_ERR)
     {
         perror("signal");
         printf("errno: %d\n", errno);
-        //fclose(fptr);
         exit(1);
     }
 
@@ -64,7 +49,6 @@ int main()
             perror("socket");
             printf("errno: %d\n", errno);
             close(sock_fd);
-            //fclose(fptr);
             exit(1);
         }
         if (connect(sock_fd, (struct sockaddr *)&srvr_addr, sizeof(srvr_addr)) == -1)
@@ -72,35 +56,29 @@ int main()
             perror("connect");
             printf("%d) errno: %d\n", i, errno);
             close(sock_fd);
-            //fclose(fptr);
             exit(1);
         }
 
-        //clock_gettime(CLOCK_REALTIME, &p_starttime);
         if (write(sock_fd, &clnt_type, sizeof(clnt_type)) == -1)
         {
             perror("write");
             printf("errno: %d\n", errno);
             close(sock_fd);
-            //fclose(fptr);
             exit(1);
         }
         
         read_len = read(sock_fd, buf, sizeof(buf));
-        //clock_gettime(CLOCK_REALTIME, &p_endtime);
         if (read_len == -1)
         {
             perror("read");
             printf("errno: %d\n", errno);
             close(sock_fd);
-            //fclose(fptr);
             exit(1);
         }
         else if (read_len == 0)
         {
             printf("connection terminated\n");
             close(sock_fd);\
-            //fclose(fptr);
             exit(1);
         }
         else
@@ -110,15 +88,8 @@ int main()
             printf("Type: %c. Result: %s\n", clnt_type, buf);
         }
 
-        // msec = (p_endtime.tv_sec - p_starttime.tv_sec) * 1.0e+3 + (p_endtime.tv_nsec - p_starttime.tv_nsec) / 1.0e+6;
-        // msec_sum += msec;
-        // fprintf(fptr, "TYPE: %c. RESULT: %s RQ_TIME:%lf \n", clnt_type, buf, msec);
-
         close(sock_fd);
     }
 
-    // printf("AVG_TIME: %lf\n", (i > 0) ? (msec_sum / i) : 0.0);
-
-    //fclose(fptr);
     return 0;
 }
